10bdcfbb96fcceaa5d94dd7f72c59a912339cbbe
[angoor2002] <angoorcodes@gmail.com>
2024-01-24 11:44:10 +0530

    should work

--- run.py
+++ run.py
@@ -1,26 +1,26 @@
 import pandas as pd
 import pickle
 import argparse
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import io
 import pandas as pd
 import numpy as np
-import seaborn as sns
+# import seaborn as sns
 import tensorflow as tf
 
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-from math import sqrt
-from sklearn.preprocessing import StandardScaler
-from tensorflow import keras
+# from sklearn import metrics
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LinearRegression
+# from sklearn.metrics import mean_squared_error
+# from sklearn.preprocessing import OneHotEncoder
+# from sklearn.compose import ColumnTransformer
+# from sklearn.metrics import mean_absolute_error, mean_squared_error
+# from math import sqrt
+# from sklearn.preprocessing import StandardScaler
+# from tensorflow import keras
 
 def load_model():
     # Load the pre-trained model
@@ -39,7 +39,7 @@ def make_predictions(model,test_data):
     transformers=[
         ('encoder', OneHotEncoder(),[condition_index])
     ],
-    remainder='passthrough'  # Keep the non-categorical columns as they are
+    remainder='passthrough'  
     )
     X_test = pd.DataFrame(preprocessor.fit_transform(X_test), columns=preprocessor.get_feature_names_out())
 
@@ -51,9 +51,7 @@ def main():
     parser = argparse.ArgumentParser(description="Make predictions using a trained model.")
     parser.add_argument("input_file", help="Path to the input file (test_data.csv)")
     args = parser.parse_args()
-
     input_data = pd.read_csv(args.input_file)
-
     model=load_model()
     predictions=make_predictions(model,input_data)
     ids = input_data['uuid']
